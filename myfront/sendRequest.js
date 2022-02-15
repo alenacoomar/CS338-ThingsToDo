99a11128ce1909f086b404e35c75c20c9ccfbd15
Dong Yang <outskidding@gmail.com>
2022-02-15 12:14:18 -0600

    update pages

--- myfront/sendRequest.js
+++ myfront/sendRequest.js
@@ -1,23 +1,29 @@
 
 function uploadFileData() {
     let file = document.getElementById("customFile").files[0];
-    console.log("123", file);
-    const rsp = fetch( "http://3.15.51.216:8000/", {
-        method: 'POST',
-        body: file,
-    }).then(response => {
-        response.json().then(data => {
-            let table =  document.getElementById("todolist");
-            let str = "";
-            for (let i = 0; i< data.todo.length; i++) {
-                str += "<tr>";
-                    str += "<td>" + "name" + "</td>";
-                    str += "<td>" + data.todo[i].name + "</td>";
-                str += "</tr>";
-            }
-            table.innerHTML += str;
-        })
-    }).catch(err => {
-        console.log(err);
-    });
+    const reader = new FileReader();
+    let res = ""
+    reader.readAsText(file, "UTF-8");
+    reader.onload = function() {
+        res = reader.result
+
+        const rsp = fetch( "http://3.15.51.216:8000/", {
+            method: 'POST',
+            body: res,
+        }).then(response => {
+            response.json().then(data => {
+                let table =  document.getElementById("todolist");
+                let str = "";
+                for (let i = 0; i< data.todo.length; i++) {
+                    str += "<tr>";
+                        str += "<td>" + data.todo[i].name + "</td>";
+                        str += "<td>" + data.todo[i].txt[0] + "</td>";
+                    str += "</tr>";
+                }
+                table.innerHTML += str;
+            })
+        }).catch(err => {
+            console.log(err);
+        });
+    };
 }
